cdfdb256aa44a83ea4ea654b5d147d12c73584fd
Alen Pagac <alen.pagac@gmail.com>
2025-10-17 15:57:49 +0200

    kuma

--- api/token.php
+++ api/token.php
@@ -3,23 +3,19 @@
 //https://github.com/MedAziz11/Uptime-Kuma-Web-API
 
 function login()
-{// URL za autentifikaciju
+{
     $login_url = "http://127.0.0.1:8000/login/access-token/";
 
-// Korisničko ime i lozinka
     $username = "admin";
     $password = "admin";
 
-// Priprema podataka za POST zahtjev
     $data = http_build_query([
         'username' => $username,
         'password' => $password,
     ]);
 
-// Inicijalizacija cURL-a
     $ch = curl_init();
 
-// Postavljanje cURL opcija za zahtjev
     curl_setopt($ch, CURLOPT_URL, $login_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
@@ -29,18 +25,14 @@ function login()
     ]);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-// Izvršavanje zahtjeva i dobivanje odgovora
     $response = curl_exec($ch);
 
-// Provjera za greške u cURL zahtjevu
     if (curl_errno($ch)) {
         echo 'Curl error: '.curl_error($ch);
     }
 
-// Zatvaranje cURL sesije
     curl_close($ch);
 
-// Parsiranje JSON odgovora i dohvat tokena
     $response_data = json_decode($response, true);
     if (isset($response_data['access_token'])) {
         $token = $response_data['access_token'];
